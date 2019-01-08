e99cd8aea783446f82063495931474144691f1fd
latna.matyas@gmail.com <latna.matyas@gmail.com>
2019-01-08 12:41:50 +0100

    refractor code with header, footer

--- admin/index.php
+++ admin/index.php
@@ -196,15 +196,6 @@
         </div>
         <!-- /#page-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+    <?php
+        include "includes/footer.php";
+    ?>
