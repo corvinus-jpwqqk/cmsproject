8673264c5262f8d5b4b9ab3c21df21fb956cd93a
latna.matyas@gmail.com <latna.matyas@gmail.com>
2019-01-13 14:09:48 +0100

    format code

--- search.php
+++ search.php
@@ -4,28 +4,19 @@ include "includes/connect.php";
 ?>
 
 <body>
-
     <!-- Navigation -->
     <?php
         include "includes/navigation.php";
     ?>
     <!-- Page Content -->
     <div class="container">
-
         <div class="row">
-
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-                
-
-
-
-                
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
-
                 <!-- First Blog Post -->
                 <?php
                     if(isset($_POST['submit'])){
@@ -40,9 +31,7 @@ include "includes/connect.php";
                             echo "No posts with the tag!";
                         }
                         else{
-                            while($row = mysqli_fetch_assoc($search_query)){
-
-                            
+                            while($row = mysqli_fetch_assoc($search_query)){      
                             $post_title = $row['post_title'];
                             $post_author = $row['post_author'];
                             $post_date = $row['post_date'];
@@ -61,38 +50,27 @@ include "includes/connect.php";
                             <hr>
                             <p><?php echo $post_content ?></p>
                             <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                        
                             <?php
                                     }
                                 }
                             }
                             ?>
-
             </div>
-
             <!-- Blog Sidebar Widgets Column -->
             <?php 
                 include "includes/sidebar.php";
             ?>
-
         </div>
         <!-- /.row -->
-
         <hr>
         <?php
             include "includes/footer.php";
-        ?>
-        
+        ?>        
     </div>
     <!-- /.container -->
-
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
 </body>
-
 </html>
