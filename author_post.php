43f5f7138cd58b2e56b39884faddb458ead7f694
Antal Mátyás <latna.matyas@gmail.com>
2019-02-12 10:29:01 +0100

    start author_post page

---
+++ author_post.php
@@ -0,0 +1,94 @@
+<?php
+    include "includes/header.php";
+    include "includes/connect.php";
+?>
+<body>
+
+    <!-- Navigation -->
+    <?php
+        include "includes/navigation.php";
+    ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Post Content Column -->
+            <div class="col-lg-8">
+
+                <!-- Blog Post -->
+<?php
+    if(isset($_GET['pid']) && isset($_GET['author'])){
+        $postid = $_GET['pid'];
+        $post_author = $_GET['author'];
+        global $connection;
+        $getpost_query = "SELECT * FROM posts WHERE post_author='{$post_author}'";
+        $getpost_query_result = mysqli_query($connection, $getpost_query);
+        while($row = mysqli_fetch_assoc($getpost_query_result))
+        {
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+        }
+    }
+?>
+
+                <!-- Title -->
+                <h1><?php echo $post_title; ?></h1>
+
+                <!-- Author -->
+                <p class="lead">
+                    by <a href="author_post.php?author=<?php echo $post_author ?>&pid=<?php echo $post_id ?>"><?php echo $post_author ?><?php echo $post_author ?></a>
+                </p>
+
+                <hr>
+
+                <!-- Date/Time -->
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+
+                <hr>
+
+                <!-- Preview Image -->
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="nincen"> 
+                <hr>
+
+                <!-- Post Content -->
+                
+                <p><?php echo $post_content; ?></p>
+
+                <hr>
+        <?php
+            include "includes/comments.php";
+        ?>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+                    <?php
+                        include "includes/sidebar.php";
+                    ?>
+                    </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <?php
+            include "includes/footer.php";
+        ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -37,7 +37,7 @@ include "includes/connect.php";
                     <a href="post.php?postid=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                     </h2>
                     <p class="lead">
-                        by <a href="index.php"><?php echo $post_author ?></a>
+                        by <a href="author_post.php?author=<?php echo $post_author ?>&pid=<?php echo $post_id ?>"><?php echo $post_author ?></a>
                     </p>
                     <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                     <hr>
