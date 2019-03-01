f52daf06a10aea1d5914efa952fd4559db5bf316
Antal Mátyás <latna.matyas@gmail.com>
2019-03-01 09:02:53 +0100

    create online users functionality

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,28 @@
 <!-- ADMIN INDEX -->
 <?php
     include "includes/admin_header.php";
+    include "..includes/connect.php";
 ?>
+<?php
+global $connection;
+$session = session_id();
+$time = time();
+$time_out_in_seconds = 60;
+$time_out = $time - $time_out_in_seconds;
+$query = "SELECT * FROM users_online WHERE session='$session'";
+$send_query = mysqli_query($connection, $query);
+$count = mysqli_num_rows($send_query);
+if($count == NULL){
+    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES ('$session', '$time')");
+}
+else{
+    mysqli_query($connection, "UPDATE users_online SET time='$time' WHERE session='$session'");
+}
+$users_online_query = "SELECT * FROM users_online WHERE time > '$time_out'";
+$users_online = mysqli_query($connection, $users_online_query);
+$count_user = mysqli_num_rows($users_online);
+?>
+
 
 <body>
     <div id="wrapper">
@@ -17,7 +38,9 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
+                            <?php
+                                echo $count_user;
+                            ?>
                             
                             <small>Subheading</small>
                         </h1>
